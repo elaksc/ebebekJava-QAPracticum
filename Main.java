55a7adf71b28e0b73d1b937dd4e1f1c7cafbe91e
Ela Kaşcıoglu <77547429+elaksc@users.noreply.github.com>
2022-10-25 00:21:10 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,9 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-
         Employee employee = new Employee("Ela",2000D,45D,1985);
         System.out.println(employee.toString());
-
-
     }
 }
